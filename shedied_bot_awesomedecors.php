423a18f387c18a01641f64d0cad78ccfbc03f15c
kelaskakap <fikri.rijalul@gmail.com>
2020-03-04 09:39:30 +0700

    parser awesome udpate

--- shedied_bot_awesomedecors.php
+++ shedied_bot_awesomedecors.php
@@ -61,14 +61,135 @@ function bot_awesomedecors_4() {
     //update_first_Run('homedesigning_13', $helper->arrFirstRun());
 }
 
-add_action('bot_awesomedecors_4', 'bot_awesomedecors_4');
+function bot_awesomedecors_5() {
+
+    $sources = SheDieDConfig::pick_Sources([22, 23, 24, 25, 26], [6785, 6786, 6787, 6788, 6789]);
+
+    $fr = first_Run('onekindesign_1');
+    $helper = new Dua();
+    $helper->yesFirstRun($fr);
+
+    shedied_exec_bot($helper, $sources, 10, 'tsnt_awesomedecors_21', false);
+
+    update_first_Run('onekindesign_1', $helper->arrFirstRun());
+}
+
+add_action('bot_awesomedecors_5', 'bot_awesomedecors_5');
+
+function bot_awesomedecors_6() {
+
+    $sources = SheDieDConfig::pick_Sources([28, 29, 30, 31, 32], [6790, 6791, 6792, 8, 6794]);
+
+    $fr = first_Run('onekindesign_2');
+    $helper = new Dua();
+    $helper->yesFirstRun($fr);
+
+    shedied_exec_bot($helper, $sources, 10, 'tsnt_awesomedecors_22', false);
+
+    update_first_Run('onekindesign_2', $helper->arrFirstRun());
+}
+
+add_action('bot_awesomedecors_6', 'bot_awesomedecors_6');
+
+function bot_awesomedecors_7() {
+
+    $sources = SheDieDConfig::pick_Sources([33, 34, 35, 36, 37], [6795, 6796, 6798, 6799, 6800]);
+
+    $fr = first_Run('onekindesign_3');
+    $helper = new Dua();
+    $helper->yesFirstRun($fr);
+
+    shedied_exec_bot($helper, $sources, 10, 'tsnt_awesomedecors_23', false);
+
+    update_first_Run('onekindesign_3', $helper->arrFirstRun());
+}
+
+add_action('bot_awesomedecors_7', 'bot_awesomedecors_7');
+
+function bot_awesomedecors_8() {
+
+    $sources = SheDieDConfig::pick_Sources([38, 39, 40, 41, 42], [6801, 6802, 3, 6804, 14]);
+
+    $fr = first_Run('onekindesign_4');
+    $helper = new Dua();
+    $helper->yesFirstRun($fr);
+
+    shedied_exec_bot($helper, $sources, 10, 'tsnt_awesomedecors_24', false);
+
+    update_first_Run('onekindesign_4', $helper->arrFirstRun());
+}
+
+add_action('bot_awesomedecors_8', 'bot_awesomedecors_8');
+
+function bot_awesomedecors_9() {
+
+    $sources = SheDieDConfig::pick_Sources([43, 44, 45, 46, 47], [6805, 16, 6812, 6813, 6814]);
+
+    $fr = first_Run('onekindesign_5');
+    $helper = new Dua();
+    $helper->yesFirstRun($fr);
+
+    shedied_exec_bot($helper, $sources, 10, 'tsnt_awesomedecors_25', false);
+
+    update_first_Run('onekindesign_5', $helper->arrFirstRun());
+}
+
+add_action('bot_awesomedecors_9', 'bot_awesomedecors_9');
+
+function bot_awesomedecors_10() {
+
+    $sources = SheDieDConfig::pick_Sources([48, 49, 50, 51, 52], [6815, 6816, 6817, 22, 6819]);
+
+    $fr = first_Run('onekindesign_6');
+    $helper = new Dua();
+    $helper->yesFirstRun($fr);
+
+    shedied_exec_bot($helper, $sources, 10, 'tsnt_awesomedecors_26', false);
+
+    update_first_Run('onekindesign_6', $helper->arrFirstRun());
+}
+
+add_action('bot_awesomedecors_10', 'bot_awesomedecors_10');
+
+function bot_awesomedecors_11() {
+
+    $sources = SheDieDConfig::pick_Sources([53, 54, 55, 56, 57], [6820, 6822, 6823, 6824, 6825]);
+
+    $fr = first_Run('onekindesign_7');
+    $helper = new Dua();
+    $helper->yesFirstRun($fr);
+
+    shedied_exec_bot($helper, $sources, 10, 'tsnt_awesomedecors_27', false);
+
+    update_first_Run('onekindesign_7', $helper->arrFirstRun());
+}
+
+add_action('bot_awesomedecors_11', 'bot_awesomedecors_11');
 
 function bot_awesomedecors_sweeper() {
 
-    shedied_exec_bot(new Dua(), [], 5, 'tsnt_awesomedecors_1', true);
-    shedied_exec_bot(new Dua(), [], 5, 'tsnt_awesomedecors_5', true);
-    shedied_exec_bot(new Dua(), [], 5, 'tsnt_awesomedecors_9', true);
-    shedied_exec_bot(new Dua(), [], 5, 'tsnt_awesomedecors_13', true);
+    //homedesigning
+    /* shedied_exec_bot(new Dua(), [], 5, 'tsnt_awesomedecors_1', true);
+      shedied_exec_bot(new Dua(), [], 5, 'tsnt_awesomedecors_5', true);
+      shedied_exec_bot(new Dua(), [], 5, 'tsnt_awesomedecors_9', true);
+      shedied_exec_bot(new Dua(), [], 5, 'tsnt_awesomedecors_13', true); */
+
+    //onekindesign
+    $now = (int) date('H');
+
+    if ($now < 13) {
+
+        //siang
+        shedied_exec_bot(new Dua(), [], 1, 'tsnt_awesomedecors_21', true);
+        shedied_exec_bot(new Dua(), [], 1, 'tsnt_awesomedecors_22', true);
+        shedied_exec_bot(new Dua(), [], 1, 'tsnt_awesomedecors_23', true);
+        shedied_exec_bot(new Dua(), [], 1, 'tsnt_awesomedecors_24', true);
+    } else {
+        //malam
+        shedied_exec_bot(new Dua(), [], 1, 'tsnt_awesomedecors_25', true);
+        shedied_exec_bot(new Dua(), [], 1, 'tsnt_awesomedecors_26', true);
+        shedied_exec_bot(new Dua(), [], 1, 'tsnt_awesomedecors_27', true);
+    }
 }
 
 add_action('bot_awesomedecors_sweeper', 'bot_awesomedecors_sweeper');
